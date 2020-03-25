f004810bb922dee82823fa324a6ec8d6cb245b20
Andreas Barth <barth.andreas@web.de>
2020-03-25 10:19:08 +0100

    1st change

---
+++ git_math.py
@@ -0,0 +1,19 @@
+x = 7
+y = 3
+
+def Addition(x,y):
+	return x+y
+
+def Subtraction(x,y):
+	return x-y 
+	
+def Mulitplication(x,y):
+	pass
+    
+def Division(x,y):
+	pass 
+	
+def PowerLaw(x,y):
+	pass 
+	
+## New comment
